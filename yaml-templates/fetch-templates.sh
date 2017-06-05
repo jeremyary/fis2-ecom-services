28cd1f41ce2c36532dd0b2382fcc4a18e143bf8c
Jeremy Ary <jary@redhat.com>
2017-06-05 15:21:55 -0500

    changing spring boot version back to 1.4.1 to test pom change

--- yaml-templates/fetch-templates.sh
+++ yaml-templates/fetch-templates.sh
@@ -5,8 +5,6 @@ cd yaml-templates
 curl -O -L https://raw.githubusercontent.com/jeremyary/fis2-ecom-services/master/yaml-templates/billing-template.yaml
 curl -O -L https://raw.githubusercontent.com/jeremyary/fis2-ecom-services/master/yaml-templates/gateway-template.yaml
 curl -O -L https://raw.githubusercontent.com/jeremyary/fis2-ecom-services/master/yaml-templates/messaging-template.yaml
-curl -O -L https://raw.githubusercontent.com/jeremyary/fis2-ecom-services/master/yaml-templates/presentation-template
-.yaml
 curl -O -L https://raw.githubusercontent.com/jeremyary/fis2-ecom-services/master/yaml-templates/product-template.yaml
 curl -O -L https://raw.githubusercontent.com/jeremyary/fis2-ecom-services/master/yaml-templates/sales-template.yaml
 curl -O -L https://raw.githubusercontent.com/jeremyary/fis2-ecom-services/master/yaml-templates/warehouse-template.yaml
